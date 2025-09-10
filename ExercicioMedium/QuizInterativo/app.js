f4089a45f86d736a7d48ad39c7eab8abfe0001e3
Wesley Figueiredo <weslk1111@gmail.com>
2025-09-10 14:13:39 -0300

    Incluindo novo exercício (QuisInterativo)

---
+++ ExercicioMedium/QuizInterativo/app.js
@@ -0,0 +1,11 @@
+function quiz( ...perguntas){
+// if(typeof qtdPergunta !== "number") throw new Error("qtdPergunta não é do Tipo Number");
+const pergunta = [...perguntas];
+const respostas = [];
+
+return console.log(...pergunta);
+
+}
+
+quiz("Qual a capital do Brasil?","Qual a capital do Japão?");
+
