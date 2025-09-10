3d2412f51e057403a5299bd19cea9ce4068f3dc1
Wesley Figueiredo <weslk1111@gmail.com>
2025-09-10 15:52:18 -0300

    Finalização de Quiz Interativo!

--- ExercicioMedium/QuizInterativo/app.js
+++ ExercicioMedium/QuizInterativo/app.js
@@ -1,11 +1,63 @@
-function quiz( ...perguntas){
-// if(typeof qtdPergunta !== "number") throw new Error("qtdPergunta não é do Tipo Number");
-const pergunta = [...perguntas];
-const respostas = [];
+const readline = require("readline");
 
-return console.log(...pergunta);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+const perguntas = [
+  {
+    enunciado: "Qual a capital do Brasil?",
+    opcoes: {
+      a: "Brasília",
+      b: "Distrito federal",
+      c: "Santos",
+    },
+    resposta: "a",
+  },
+  {
+    enunciado: "Qual a capital do Japão?",
+    opcoes: {
+      a: "San Diego",
+      b: "Tokyo",
+      c: "Hong Kong",
+    },
+    resposta: "b",
+  },
+  {
+    enunciado: "Qual a linguagem de programação é comum em análise de dados?",
+    opcoes: {
+      a: "JavaScript",
+      b: "C#",
+      c: "Python",
+    },
+    resposta: "c",
+  },
+];
+
+let indice = 0;
+
+function perguntar() {
+  const q = perguntas[indice];
+
+  rl.question(
+    `\n${q.enunciado}\n a) ${q.opcoes.a}\n b) ${q.opcoes.b}\n c) ${q.opcoes.c}\n Sua resposta:`,
+    (resposta) => {
+      if (resposta.toLowerCase() === q.resposta) {
+        console.log("\nResposta correta!");
+      } else {
+        console.log(`\nResposta errada! A resposta correta é "${q.resposta}".`);
+      }
+
+      indice++;
+
+      if (indice < perguntas.length) {
+        perguntar();
+      } else {
+        console.log("\nFiz das questões");
+      }
+    }
+  );
 }
 
-quiz("Qual a capital do Brasil?","Qual a capital do Japão?");
-
+perguntar();
