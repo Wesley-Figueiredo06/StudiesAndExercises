351b159efc3631a70412edc11fc84538ed266570
Wesley Figueiredo <weslk1111@gmail.com>
2025-09-11 17:07:21 -0300

    Iniciando Agenda CRUD

---
+++ ExercicioMedium/Agenda_CRUD/app.js
@@ -0,0 +1,1 @@
+import Pessoa from "./scripts/models.js";
---
+++ ExercicioMedium/Agenda_CRUD/scripts/models.js
@@ -0,0 +1,9 @@
+export default class Contato {
+  constructor(nome, telefone, email) {
+    this.nome = nome;
+    this.telefone = telefone;
+    this.email = email;
+  }
+
+  
+}
